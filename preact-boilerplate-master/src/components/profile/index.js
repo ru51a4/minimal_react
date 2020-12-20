5249074e88ba851a79cfd2a79a393bccc6203a16
Ваше Имя <you@example.com>
2020-12-20 15:58:30 +0500

    aaa

---
+++ preact-boilerplate-master/src/components/profile/index.js
@@ -0,0 +1,18 @@
+import { h, Component } from 'preact';
+import style from './style.less';
+
+export default class Profile extends Component {
+	state = {
+		count: 0
+	};
+
+
+	// Note: `user` comes from the URL, courtesy of our router
+	render() {
+		return (
+			<div >
+				<div>asdasdas</div>
+			</div>
+		);
+	}
+}
