bc0ce5842ba02a8c8b2708ce733b99c892b27559
ru51a4 <rruslanvaleev@yandex.ru>
2023-12-18 19:48:18 +0500

    fix

--- demo/iblockcms-frontend/components/catalog/catalog.component.js
+++ demo/iblockcms-frontend/components/catalog/catalog.component.js
@@ -12,12 +12,13 @@ class component_catalog extends component {
                 <ul>
                 ${this.state?.els?.reduce((acc, item, i) => acc +
             `
-                                <li kek="${item.name}" class="card mb-4 d-flex justify-content-center">
+                                <li class="card mb-4 d-flex justify-content-center">
                                     <div class="p-2">
                                         <span
                                             >${item.name}</span>
                                         <p>
                                             <img style="width:100px" src="http://iblock.1123875-cc97019.tw1.ru${item.prop['DETAIL_PICTURE']}">
+                                            </img>
                                         </p>
                                         <ul>
                                         ${Object.keys(item?.prop)?.map((key) => {
--- render.js
+++ render.js
@@ -269,6 +269,7 @@ class render {
 
             }
             deepReplace(this.vdom[0].id);
+            console.log({ stackUpdateDom })
             stackUpdateDom.forEach((itemUpdate) => {
                 let domEl = getDomEl([...itemUpdate.prev.parent, itemUpdate.prev.id]);
                 html = '';
