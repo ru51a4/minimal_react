c3424dff90773266c77be85f34f380bdad0aba18
ru51a4 <rruslanvaleev@yandex.ru>
2024-07-14 17:23:03 +0500

    deploy

--- landing/demo/iblockcms-frontend/api.js
+++ landing/demo/iblockcms-frontend/api.js
@@ -1,11 +1,27 @@
 
 class Api {
-    static apiUrl = "https://crossorigin.me/http://188.120.245.72:8082/api";
+    static apiUrl = "https://proxy.cors.sh//http://188.120.245.72:8082/api";
     static getCatalog = (sectionId, page) => {
-        return axios.get(`${this.apiUrl}/index/${sectionId}/${page}`)
+        return axios.get(`${this.apiUrl}/index/${sectionId}/${page}`, {
+
+            headers: {
+
+                'x-cors-api-key': 'temp_c1363218646395fd3f0af43e15fbae42'
+
+            }
+
+        })
     }
     static getProduct = (id) => {
-        return axios.get(`${this.apiUrl}/detail/${id}`)
+        return axios.get(`${this.apiUrl}/detail/${id}`, {
+
+            headers: {
+
+                'x-cors-api-key': 'temp_c1363218646395fd3f0af43e15fbae42'
+
+            }
+
+        })
     }
 
 
